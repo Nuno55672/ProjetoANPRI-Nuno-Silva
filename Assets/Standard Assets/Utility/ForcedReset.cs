ce5460dc1602401a6082022aba25038aa4f4d8a0
Nuno Silva <nuno.silva6072@gmail.com>
2020-04-30 23:31:26 +0100

    update 2

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
-
+[RequireComponent(typeof(Image))]
 #pragma warning disable 618
-[RequireComponent(typeof (Image))]
 public class ForcedReset : MonoBehaviour
 {
     private void Update()
